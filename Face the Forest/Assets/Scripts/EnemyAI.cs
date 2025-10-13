30ff4887f36e9a0a5fdcef508a7659b107936b8a
ChrisTuck04 <christopher@tuckman.us>
2025-10-13 18:48:36 -0400

    enemy waypoint system integration

--- Face the Forest/Assets/Scripts/EnemyAI.cs
+++ Face the Forest/Assets/Scripts/EnemyAI.cs
@@ -8,19 +8,21 @@ public class EnemyAI : MonoBehaviour
     public Transform target;
     public List<Transform> destinations;
     //public Animator anim;
-    public float walkSpeed, chaseSpeed, minIdleTime, maxIdleTime, idleTime;
+    public float walkSpeed, chaseSpeed, minIdleTime, maxIdleTime;
+    float idleTime;
     public bool walking, chasing;
     public Transform player;
     Transform currentDest;
     Vector3 dest;
-    int randDest, randDecision;
+    int randDest, randDecision, destRandCount;
     public int destinationAmount;
     public NavMeshAgent agent;
 
     void Start()
     {
+        destRandCount = destinationAmount + 1;
         walking = true;
-        randDest = Random.Range(0, destinationAmount);
+        randDest = Random.Range(0, destRandCount);
         currentDest = destinations[randDest];
         agent = GetComponent<NavMeshAgent>();
     }
@@ -37,15 +39,15 @@ public class EnemyAI : MonoBehaviour
                 randDecision = Random.Range(0, 2);
                 if (randDecision == 0)
                 {
-                    randDest = Random.Range(0, destinationAmount);
+                    randDest = Random.Range(0, destRandCount);
                     currentDest = destinations[randDest];
                 }
                 if (randDecision == 1)
                 {
                     //anim.ResetTrigger("walk");
                     //anim.SetTrigger("idle");
-                    StopCoroutine("stayIdle");
-                    StartCoroutine("stayIdle");
+                    StopCoroutine("StayIdle");
+                    StartCoroutine("StayIdle");
                     walking = false;
                 }
 
@@ -54,12 +56,12 @@ public class EnemyAI : MonoBehaviour
             }
         }
     }
-    IEnumerator stayIdle()
+    IEnumerator StayIdle()
     {
         idleTime = Random.Range(minIdleTime, maxIdleTime);
         yield return new WaitForSeconds(idleTime);
         walking = true;
-        randDest = Random.Range(0, destinationAmount);
+        randDest = Random.Range(0, destRandCount);
         currentDest = destinations[randDest];
         //anim.ResetTrigger("idle");
         //anim.SetTrigger("walk");
